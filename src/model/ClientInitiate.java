f6b11a0d4ea484931b4f52661e48632c2a09d41d
Omar <omar_rahman_@bitbucket.org>
2016-06-16 02:54:29 +0400

    added displaying message sent functionality built server to handle incoming and outgoing requests

--- src/model/ClientInitiate.java
+++ src/model/ClientInitiate.java
@@ -4,7 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -12,37 +14,91 @@ import java.net.Socket;
 /**
  * Created by Omar on 6/12/2016.
  */
+/**
+ * lets client launch their own chat app
+ * and send messages to server (localhost)
+ */
+
 public class ClientInitiate extends JFrame {
 
     private JTextArea messageToSend;
     private JButton sendMessage;
     private Socket socket;
     private PrintWriter printWriter;
+    private JPanel conversation;
 
-    public ClientInitiate() {
+    public void go() {
         setUpNetworking();
+        setUpUI();
+    }
+
+    /**
+     * sets UI for application
+     * adds networking functionality to send message
+     */
+    private void setUpUI() {
         setLayout(new BorderLayout());
-        JPanel sendPanel = new JPanel();
-        sendPanel.setLayout(new BoxLayout(sendPanel, BoxLayout.X_AXIS));
-        sendPanel.add(messageToSend = new JTextArea());
-        sendPanel.add(sendMessage = new JButton("Send"));
-        add(sendPanel, BorderLayout.SOUTH);
+        setSendFrame();
+        setConversation();
         setSize(new Dimension(800,600));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         sendMessage.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                sendMessageNow();
+                try {
+                    receiveNewMessages();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            //receive messages from server
+            private void receiveNewMessages() throws IOException {
+                BufferedReader bufferedReader = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream())
+                );
+                String message;
+                while ((message=bufferedReader.readLine())!=null){
+                    conversation.add(new JLabel(message));
+                    repaint();
+                    revalidate();
+                }
+            }
+            //send message to server
+            private void sendMessageNow() {
                 printWriter.println(messageToSend.getText());
                 printWriter.flush();
                 messageToSend.setText("");
                 messageToSend.requestFocus();
             }
         });
+
         messageToSend.requestFocus();
-        setVisible(true);
     }
 
+    /**
+     * sets up UI for conversation pane
+     */
+    private void setConversation() {
+        (conversation = new JPanel()).setLayout(new BoxLayout(conversation, BoxLayout.Y_AXIS));
+        add(new JScrollPane(conversation), BorderLayout.CENTER);
+    }
+
+    /**
+     * configures bottom part of window to send messages
+     */
+    private void setSendFrame() {
+        JPanel sendPanel = new JPanel();
+        sendPanel.setLayout(new BoxLayout(sendPanel, BoxLayout.X_AXIS));
+        sendPanel.add(messageToSend = new JTextArea());
+        sendPanel.add(sendMessage = new JButton("Send"));
+        add(sendPanel, BorderLayout.SOUTH);
+    }
+
+    /**
+     * creates socket
+     */
     private void setUpNetworking() {
         try {
             socket = new Socket("127.0.0.1", 4242);
@@ -53,6 +109,8 @@ public class ClientInitiate extends JFrame {
     }
 
     public static void main(String[] args) {
-        new ClientInitiate();
+        ClientInitiate clientInitiate = new ClientInitiate();
+        clientInitiate.go();
+        clientInitiate.setVisible(true);
     }
 }
--- src/model/ServerInitiate.java
+++ src/model/ServerInitiate.java
@@ -4,13 +4,49 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * Created by Omar on 6/12/2016.
  */
 public class ServerInitiate {
 
+    private ArrayList messages;
+    /**
+     * launches server
+     */
     public void go() {
 
+        try(ServerSocket serverSocket = new ServerSocket(4242)){
+            while (true){
+                Socket socket = serverSocket.accept();
+                retrieveMessages(socket);
+                sendMessages(socket);
+            }
+        }
+        catch (IOException e){}
+    }
+
+    private void retrieveMessages(Socket socket) throws IOException{
+        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String message;
+        while ((message=bufferedReader.readLine())!=null){
+            messages.add(message);
+        }
+        inputStreamReader.close();
+    }
+
+    private void sendMessages(Socket socket) throws IOException{
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        for (String s: (ArrayList<String>) messages){
+            printWriter.println(s+" - "+System.currentTimeMillis());
+        }
+        printWriter.close();
+        messages.clear();
+    }
+
+    public static void main(String[] args) {
+        new ServerInitiate().go();
     }
 }
